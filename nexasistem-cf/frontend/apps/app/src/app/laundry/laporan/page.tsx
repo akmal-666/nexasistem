dc1ff4276b22b31d65cea4f1b8d53a584e0bdffa
akmal-666 <bkhrakmal@gmail.com>
2026-04-09 00:41:40 +0700

    deleted all

--- nexasistem-cf/frontend/apps/app/src/app/laundry/laporan/page.tsx
+++
@@ -1,5 +0,0 @@
-import LaporanPage from '@/components/laporan/LaporanPage'
-
-export default function Page() {
-  return <LaporanPage module="laundry" title="Laporan Laundry" />
-}
--- nexasistem-cf/frontend/apps/app/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import { useEffect } from 'react'
-export default function HomePage() {
-  useEffect(() => { window.location.href = '/login' }, [])
-  return null
-}
